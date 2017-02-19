295d2d189fd454391132fd4121381483f42725c1
ManHyuk <10000hyuk@naver.com>
2017-02-19 19:52:32 +0900

    first commit

---
+++ convert_ctr.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MetroUI_ver2
+{
+    partial class convert_ctr
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroListView1 = new MetroFramework.Controls.MetroListView();
+            this.metroListView2 = new MetroFramework.Controls.MetroListView();
+            this.metroListView3 = new MetroFramework.Controls.MetroListView();
+            this.metroListView4 = new MetroFramework.Controls.MetroListView();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroListView1
+            // 
+            this.metroListView1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.metroListView1.FullRowSelect = true;
+            this.metroListView1.Location = new System.Drawing.Point(48, 78);
+            this.metroListView1.Name = "metroListView1";
+            this.metroListView1.OwnerDraw = true;
+            this.metroListView1.Size = new System.Drawing.Size(198, 400);
+            this.metroListView1.TabIndex = 0;
+            this.metroListView1.UseCompatibleStateImageBehavior = false;
+            this.metroListView1.UseSelectable = true;
+            // 
+            // metroListView2
+            // 
+            this.metroListView2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.metroListView2.FullRowSelect = true;
+            this.metroListView2.Location = new System.Drawing.Point(285, 78);
+            this.metroListView2.Name = "metroListView2";
+            this.metroListView2.OwnerDraw = true;
+            this.metroListView2.Size = new System.Drawing.Size(198, 400);
+            this.metroListView2.TabIndex = 1;
+            this.metroListView2.UseCompatibleStateImageBehavior = false;
+            this.metroListView2.UseSelectable = true;
+            // 
+            // metroListView3
+            // 
+            this.metroListView3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.metroListView3.FullRowSelect = true;
+            this.metroListView3.Location = new System.Drawing.Point(522, 78);
+            this.metroListView3.Name = "metroListView3";
+            this.metroListView3.OwnerDraw = true;
+            this.metroListView3.Size = new System.Drawing.Size(198, 400);
+            this.metroListView3.TabIndex = 2;
+            this.metroListView3.UseCompatibleStateImageBehavior = false;
+            this.metroListView3.UseSelectable = true;
+            // 
+            // metroListView4
+            // 
+            this.metroListView4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.metroListView4.FullRowSelect = true;
+            this.metroListView4.Location = new System.Drawing.Point(759, 78);
+            this.metroListView4.Name = "metroListView4";
+            this.metroListView4.OwnerDraw = true;
+            this.metroListView4.Size = new System.Drawing.Size(198, 400);
+            this.metroListView4.TabIndex = 3;
+            this.metroListView4.UseCompatibleStateImageBehavior = false;
+            this.metroListView4.UseSelectable = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(120, 43);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(54, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Normal";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(364, 43);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(40, 19);
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "Event";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(604, 43);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(34, 19);
+            this.metroLabel3.TabIndex = 6;
+            this.metroLabel3.Text = "Park";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(834, 43);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(49, 19);
+            this.metroLabel4.TabIndex = 7;
+            this.metroLabel4.Text = "Picture";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(461, 505);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 8;
+            this.metroButton1.Text = "Convert";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // convert_ctr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroListView4);
+            this.Controls.Add(this.metroListView3);
+            this.Controls.Add(this.metroListView2);
+            this.Controls.Add(this.metroListView1);
+            this.Name = "convert_ctr";
+            this.Size = new System.Drawing.Size(1000, 550);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroListView metroListView1;
+        private MetroFramework.Controls.MetroListView metroListView2;
+        private MetroFramework.Controls.MetroListView metroListView3;
+        private MetroFramework.Controls.MetroListView metroListView4;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
